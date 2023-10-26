3a57376e51ad753270eb093309b524be47902030
wood3n <wangkka1@163.com>
2023-10-26 23:08:16 +0800

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,29 @@ const { defineConfig } = require('eslint-define-config');
 module.exports = defineConfig({
   root: true,
   env: {
+    es2022: true,
     browser: true,
     node: true, 
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/strict-type-checked',],
   plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
     ecmaVersion: 2022,
+    project: [
+      './tsconfig.json',
+      './packages/*/tsconfig.json'
+    ],
   },
-  rules: {}
+  overrides: [
+    {
+      files: ['*.js'],
+      extends: ['plugin:@typescript-eslint/disable-type-checked']
+    },
+  ],
+  rules: {
+    '@typescript-eslint/no-var-requires': 0
+  },
 });
