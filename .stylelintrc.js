fb1881b3803ad3751184a1d41efeb22d6ecaafcd
wood3n <wangkka1@163.com>
2024-03-13 15:20:11 +0800

    chore(stylelint): remove stylelint less configuration

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,13 +1,6 @@
 module.exports = {
   extends: ['stylelint-config-standard'],
   plugins: ['stylelint-prettier'],
-  customSyntax: 'postcss-less',
-  overrides: [
-    {
-      files: ['*.less', '**/*.less'],
-      customSyntax: 'postcss-less',
-    },
-  ],
   rules: {
     'rule-empty-line-before': [
       'always',
